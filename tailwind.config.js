ac55390106c8a3fd45487098c9cf0ae5d08c9be8
jakiasultana2022 <rokeyasbd@gmail.com>
2022-05-29 21:55:38 +0600

    initial-setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#000000",
+          secondary: "#eb3c62",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
