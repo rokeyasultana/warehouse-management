c4a901ba2f3f0a983478a99d1df8da8e84b43414
jakiasultana2022 <rokeyasbd@gmail.com>
2022-06-07 22:03:46 +0600

    login

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
    <Route path='/blogs' element={<Blogs/>}>Blogs</Route>
    <Route path='/about' element={<About/>}>About</Route>
    
-   
+   <Route path='/login' element={<Login/>}>Login</Route>
 
 
    <Route path='*' element={<NotFound/>}></Route>
